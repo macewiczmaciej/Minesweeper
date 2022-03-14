e7242d6f132c28e159ce0f0df82f1c28e49af8ef
macewiczmaciej <macewicz.maciej@gmail.com>
2022-03-14 23:33:53 +0100

    gameField created and filled

--- com/codegym/games/minesweeper/MinesweeperGame.java
+++ com/codegym/games/minesweeper/MinesweeperGame.java
@@ -4,8 +4,19 @@ import com.codegym.engine.cell.*;
 
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
 
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+        createGame();
+    }
+
+    private void createGame() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField.length; j++) {
+                gameField[j][i] = new GameObject(i,j);
+                setCellColor(i, j, Color.ORANGE);
+            }
+        }
     }
 }
